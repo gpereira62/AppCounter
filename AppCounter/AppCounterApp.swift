5ef45a2ab5f8b236a89a1ff904e26e8e5cf30f7a
Gustavo Pereira <gustavopereirasantos@hotmail.com>
2022-04-05 22:54:03 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Gustavo Pereira on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
